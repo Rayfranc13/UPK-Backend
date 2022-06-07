f9e1d63e025c092b050f21a3f9398f86b82ea192
Andres Pastrana <andreserluis@gmail.com>
2022-06-07 10:34:43 -0700

    Default PORT of the server changed

--- models/server.js
+++ models/server.js
@@ -1,38 +1,38 @@
-const express= require('express')
+const express = require('express')
 const cors = require('cors');
 
-class Server{
-    constructor(){
-        this.app=express()
-        this.port=process.env.PORT
-        this.ConectarDB() 
+class Server {
+    constructor() {
+        this.app = express()
+        this.port = process.env.PORT
+        this.ConectarDB()
         this.Middlewares();
         this.Routes();
         this.Listen();
     }
 
-    Middlewares(){
-        this.app.use(express.urlencoded({extended:false}))
+    Middlewares() {
+        this.app.use(express.urlencoded({ extended: false }))
         this.app.use(express.static('public'))
         this.app.use(express.json())
         this.app.use(cors())
     }
 
-    Routes(){
+    Routes() {
 
-       
-        }
-        async ConectarDB(){
-            
-        }
 
-        Listen(){
+    }
+    async ConectarDB() {
 
-            this.app.listen(this.port||5000,()=>
-            console.log(`Server on port:${this.port}`))
-        
-        }
+    }
+
+    Listen() {
+        const defaultPort = 5000;
+        this.app.listen(this.port || defaultPort, () =>
+            console.log(`Server running on port :  ${this.port || defaultPort}`))
+
+    }
 
 }
 
-module.exports=Server
+module.exports = Server
